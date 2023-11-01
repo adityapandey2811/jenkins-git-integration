281436b28777cf8d68fc4477f4cffda1a65081b1
Aditya Pandey <2811adityapandey@gmail.com>
2023-11-01 01:35:13 -0700

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,5 @@
 public class Welcome {
     public static void main (String []args) {
-            System.out.println("Welcome Java");
+            System.out.println("Welcome Java... Hello Everyone");
     }
 }
